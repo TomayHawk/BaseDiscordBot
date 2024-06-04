2b1005024dadb4ebb3a1db14ec04aacdb96e2719
TomayHawk <gmtomayhawk@gmail.com>
2024-06-04 13:57:02 -0400

    Added Basic Slash Commands and Event Handling

---
+++ commands/utility/ping.js
@@ -0,0 +1,12 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
+};
